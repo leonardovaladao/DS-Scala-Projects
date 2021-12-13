ec84ebf51cd45b8b2dc167932cb830c119d2125a
Leonardo Valadão <leonardo.m.valadao@hotmail.com>
2021-12-13 13:59:15 -0300

    Added AUC function

--- AdvancedAnalyticsWithSpark/MusicRecommender/main.scala
+++ AdvancedAnalyticsWithSpark/MusicRecommender/main.scala
@@ -115,3 +115,97 @@ artistByID.filter($"id" isin (recommendedArtistIDs:_*)).show()
 println("Results seems fine, but could definitely be better....")
 
 println("\n====================================================\n")
+
+println("Import libraries for AUC...")
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.mllib.recommendation._
+import org.apache.spark.rdd.RDD
+
+println("Define area under curve (AUC) function...")
+// This function was taken from https://github.com/mauropelucchi/machine-learning-course/blob/master/collaborative-filtering/recommending_music.scala
+def areaUnderCurve(
+      positiveData: RDD[Rating],
+      bAllItemIDs: Broadcast[Array[Int]],
+      predictFunction: (RDD[(Int,Int)] => RDD[Rating])) = {
+    // What this actually computes is AUC, per user. The result is actually something
+    // that might be called "mean AUC".
+
+    // Take held-out data as the "positive", and map to tuples
+    val positiveUserProducts = positiveData.map(r => (r.user, r.product))
+    // Make predictions for each of them, including a numeric score, and gather by user
+    val positivePredictions = predictFunction(positiveUserProducts).groupBy(_.user)
+
+    // BinaryClassificationMetrics.areaUnderROC is not used here since there are really lots of
+    // small AUC problems, and it would be inefficient, when a direct computation is available.
+
+    // Create a set of "negative" products for each user. These are randomly chosen
+    // from among all of the other items, excluding those that are "positive" for the user.
+    val negativeUserProducts = positiveUserProducts.groupByKey().mapPartitions {
+      // mapPartitions operates on many (user,positive-items) pairs at once
+      userIDAndPosItemIDs => {
+        // Init an RNG and the item IDs set once for partition
+        val random = new Random()
+        val allItemIDs = bAllItemIDs.value
+        userIDAndPosItemIDs.map { case (userID, posItemIDs) =>
+          val posItemIDSet = posItemIDs.toSet
+          val negative = new ArrayBuffer[Int]()
+          var i = 0
+          // Keep about as many negative examples per user as positive.
+          // Duplicates are OK
+          while (i < allItemIDs.size && negative.size < posItemIDSet.size) {
+            val itemID = allItemIDs(random.nextInt(allItemIDs.size))
+            if (!posItemIDSet.contains(itemID)) {
+              negative += itemID
+            }
+            i += 1
+          }
+          // Result is a collection of (user,negative-item) tuples
+          negative.map(itemID => (userID, itemID))
+        }
+      }
+    }.flatMap(t => t)
+    // flatMap breaks the collections above down into one big set of tuples
+
+    // Make predictions on the rest:
+    val negativePredictions = predictFunction(negativeUserProducts).groupBy(_.user)
+
+    // Join positive and negative by user
+    positivePredictions.join(negativePredictions).values.map {
+      case (positiveRatings, negativeRatings) =>
+        // AUC may be viewed as the probability that a random positive item scores
+        // higher than a random negative one. Here the proportion of all positive-negative
+        // pairs that are correctly ranked is computed. The result is equal to the AUC metric.
+        var correct = 0L
+        var total = 0L
+        // For each pairing,
+        for (positive <- positiveRatings;
+             negative <- negativeRatings) {
+          // Count the correctly-ranked pairs
+          if (positive.rating > negative.rating) {
+            correct += 1
+          }
+          total += 1
+        }
+        // Return AUC: fraction of pairs ranked correctly
+        correct.toDouble / total
+    }.mean() // Return mean AUC over users
+  }
+
+println("Split trainData to get test data...")
+val allData = buildCounts(rawUserArtistData, bArtistAlias)
+val Array(trainData, cvData) = allData.randomSplit(Array(0.9, 0.1))
+trainData.cache()
+cvData.cache()
+
+val allArtistIDs = allData.select("artist").as[Int].distinct().collect()
+val bAllArtistIDs = spark.sparkContext.broadcast(allArtistIDs) 
+
+import org.apache.spark.ml.recommendation._
+
+val model = new ALS().
+  setSeed(Random.nextLong()).
+  setImplicitPrefs(true).
+  setRank(10).setRegParam(0.01).setAlpha(1.0).setMaxIter(5).
+  setUserCol("user").setItemCol("artist").
+  setRatingCol("count").setPredictionCol("prediction").
+  fit(trainData)
